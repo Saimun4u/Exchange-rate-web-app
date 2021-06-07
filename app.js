53912e32b1ef0f10cf1374d00d2d611bb033acae
Saimun <saimun4u@gmail.com>
2021-06-07 17:35:22 -0500

    Currency exchange rate logic and UI setup done

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
+//DOM Element Selection
+
+const currencyEl_one = document.getElementById("currency-one");
+const amountEl_one = document.getElementById("amount-one");
+const currencyEl_two = document.getElementById("currency-two");
+const amountEl_two = document.getElementById("amount-two");
+
+const rateEl = document.getElementById("rate");
+const swap = document.getElementById("swap")
+
+//Get exchange rates and update the DOM
+
 function calculate(){
-    fetch("items.json")
+
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    console.log(currency_one, currency_two);
+
+    fetch(`https://v6.exchangerate-api.com/v6/4f1fb9c0dc61d665f9dd0fec/latest/${currency_one}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        const rate = data.conversion_rates[currency_two];
+
+        rateEl.innerText = `1 ${currency_one} = ${currency_two} ${rate.toFixed(2)}`;
+
+        const amount_two = data.conversion_rates[currency_two]
+
+        amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    })
 }
 
-calculate()
+//Event Listeners
+
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+calculate();
+
